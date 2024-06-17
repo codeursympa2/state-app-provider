e5524923059478b2980cee86f4c2b5f05e9c99e5
Abdallah ANRICHIDINE <zerodevsn@gmail.com>
2024-06-17 00:00:32 +0000

    Suppression du SafeArea dans MyCatalog

--- lib/Pages/MyCatalog/my_catalog.dart
+++ lib/Pages/MyCatalog/my_catalog.dart
@@ -14,45 +14,43 @@ class MyCatalog extends StatelessWidget{
 
     // Je recupère mon modèle de données via le Provider.
     var cart = getMyCartModel(context);
-    return SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text(title),
-            actions: [
-              IconButton(
-                tooltip: "Voir mon panier",
-                  onPressed: (){
-                    Navigator.push(context,MaterialPageRoute(
-                        builder: (BuildContext context){
-                          return const MyCart();
-                        }
-                    ));
-                  },
-                  icon: const Icon(Icons.shopping_cart)
-              )
-            ],
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(padding),
-            child: ListView.builder(
-              itemCount: products.length,
-              itemBuilder: (BuildContext context, int i) {
-                return Card(
-                  child: ListTile(
-                    title: Text(products[i].name),
-                    trailing: buttonIconFiled(Icons.shopping_cart_checkout_outlined, Colors.blueAccent, (){
-                      //Quand on clique sur un produit on l'ajoute au panier
-                      cart.addItem(products[i]);
-                      showSnackBar(context, "${products[i].name} ajouté(e) au panier.");
-                    }),
-                    leading: Text('${i+1}') ,
-                  ),
-                );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(title),
+        actions: [
+          IconButton(
+            tooltip: "Voir mon panier",
+              onPressed: (){
+                Navigator.push(context,MaterialPageRoute(
+                    builder: (BuildContext context){
+                      return const MyCart();
+                    }
+                ));
               },
+              icon: const Icon(Icons.shopping_cart)
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(padding),
+        child: ListView.builder(
+          itemCount: products.length,
+          itemBuilder: (BuildContext context, int i) {
+            return Card(
+              child: ListTile(
+                title: Text(products[i].name),
+                trailing: buttonIconFiled(Icons.shopping_cart_checkout_outlined, Colors.blueAccent, (){
+                  //Quand on clique sur un produit on l'ajoute au panier
+                  cart.addItem(products[i]);
+                  showSnackBar(context, "${products[i].name} ajouté(e) au panier.");
+                }),
+                leading: Text('${i+1}') ,
+              ),
+            );
+          },
 
-            )
-          ),
         )
+      ),
     );
   }
 }
